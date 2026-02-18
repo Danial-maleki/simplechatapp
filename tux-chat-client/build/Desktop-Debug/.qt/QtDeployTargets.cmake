4e46e7b542e09a3fc35f4a8af79df39db848a71c
Danial <132660693+Danial-maleki@users.noreply.github.com>
2026-02-18 17:45:31 +0330

    Delete tux-chat-client/build/Desktop-Debug directory
    Delete Debug/bulid

--- tux-chat-client/build/Desktop-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_tux-chat-client_FILE /home/danial/Documents/MY_CODES/qtprojects/tux-chat-client/build/Desktop-Debug/tux-chat-client)
-set(__QT_DEPLOY_TARGET_tux-chat-client_TYPE EXECUTABLE)
